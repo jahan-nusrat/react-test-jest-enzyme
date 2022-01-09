299f5245ece548489e84fa36a34cd86761888b0a
jahan-nusrat <njnusrat53@gmail.com>
2022-01-09 22:04:20 +0100

    using enzyme in a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn React Testing/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug())
 });
